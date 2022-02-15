1eed5c99da6e7124bd67ede56051c426e8e00704
Tyler Tierney <tytierney@yahoo.com>
2022-02-14 21:54:53 -0500

    Changed server to index.js and adjusted server initialization for socket io

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 const express = require("express");
-const http = require("http");
-// const cors = require("cors");
 const socketIo = require("socket.io");
-
 const PORT = process.env.PORT || 6066;
-// const index = require("./routes/index");
 
-const app = express();
-app.get("/", (req, res) => {
-  res.send({ response: "I am alive" }).status(200);
-});
-
-app.listen(9000, () => console.log("Server Running On Port: PORT"));
-
-const server = http.createServer();
+const server = express()
+  .use((req, res) => res.send({ response: "I am alive" }).status(200))
+  .listen(PORT, () => console.log(`listening on ${PORT}`));
 
 const io = socketIo(server, {
   cors: {
@@ -24,15 +15,13 @@ const io = socketIo(server, {
 
 io.on("connection", (socket) => {
   console.log("New client connected, socket ID: " + socket.id);
-
-  socket.on("message", (msg, roomId) => {
-    console.log(roomId);
+  socket.on("joinRoom", (roomId) => {
     socket.join(roomId);
+  });
+  socket.on("message", (msg, roomId) => {
     io.to(roomId).emit("message", msg, roomId);
   });
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
 });
-
-server.listen(PORT, () => console.log(`listening on port ${PORT}`));
